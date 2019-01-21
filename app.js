ef86debd5bd2195d72d96c98aaf6e6fc33815b20
[SW연구소_2019인턴]이용준 <wooooooak@daou.co.kr>
2019-01-21 14:21:56 +0900

    추가,수정,삭제 디렉터리별 요청 기능 추가

--- app.js
+++ app.js
@@ -1,45 +1,60 @@
-const express = require('express');
-const cors = require('cors');
-const fs = require('fs');
-const summary = require('./output_summary/v1_v2_summary.json');
-const dirTree = require('directory-tree');
+const express = require("express");
+const cors = require("cors");
+const fs = require("fs");
+const summary = require("./output_summary/v1_v2_summary.json");
+const dirTree = require("directory-tree");
 
 const app = express();
 app.use(cors());
-const tree = dirTree('./v1v2_compare', { attributes: [] });
+const modList = dirTree("./v1v2_mod", { attributes: [] });
+const delList = dirTree("./v1v2_del", { attributes: [] });
+const addList = dirTree("./v1v2_add", { attributes: [] });
 
-app.get('/tree', function(req, res) {
-	res.json({ tree: tree });
+app.get("/list/mod", function(req, res) {
+  console.log("mod!");
+  res.json({ tree: modList });
 });
 
-app.get('/summary', function(req, res) {
-	res.json(summary);
+app.get("/list/add", function(req, res) {
+  console.log("add!");
+  res.json({ tree: addList });
+});
+app.get("/list/del", function(req, res) {
+  console.log("del!");
+  res.json({ tree: delList });
 });
 
-app.get('/diff_file', function(req, res) {
-	// console.dir(req.query);
-	const array = fs.readFileSync(req.query.path).toString().split('\n');
-	const jsonArray = array.map((str) => {
-		const v1Line = str.slice(0, 9);
-		const v2Line = str.slice(10, 18);
-		const symbol = str.slice(19, 20);
-		const string = str.slice(20);
-		return {
-			v1Line,
-			v2Line,
-			symbol,
-			string
-		};
-	});
-	// for (i in array) {
-	// 	console.log(array[i]);
-	// }
+app.get("/summary", function(req, res) {
+  res.json(summary);
+});
 
-	res.json({ content: jsonArray });
+app.get("/diff_file", function(req, res) {
+  // console.dir(req.query);
+  const array = fs
+    .readFileSync(req.query.path)
+    .toString()
+    .split("\n");
+  const jsonArray = array.map(str => {
+    const v1Line = str.slice(0, 9);
+    const v2Line = str.slice(10, 18);
+    const symbol = str.slice(19, 20);
+    const string = str.slice(20);
+    return {
+      v1Line,
+      v2Line,
+      symbol,
+      string
+    };
+  });
+  // for (i in array) {
+  // 	console.log(array[i]);
+  // }
+
+  res.json({ content: jsonArray });
 });
 
 var server = app.listen(3001, function() {
-	console.log('Express server has started on port 3001');
+  console.log("Express server has started on port 3001");
 });
 
 // console.log(tree);
